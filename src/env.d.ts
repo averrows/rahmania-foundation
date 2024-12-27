8b84d395358e660395c04d104b74089af0f0703b
Averrous Saloom <averous009@gmail.com>
2024-12-27 21:06:22 +0700

    feat: initialize web

---
+++ src/env.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+declare namespace JSX {
+  interface IntrinsicElements {
+    [elemName: string]: any;
+  }
+} 
