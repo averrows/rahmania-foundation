517cc20e45780f7131eb4910b3d9218779e37d20
averrows <averous009@gmail.com>
2025-01-19 20:28:49 +0700

    feat: with donation view

--- src/components/DonationView.tsx
+++ src/components/DonationView.tsx
@@ -5,39 +5,41 @@ interface Donation {
   amount: number;
   message: string;
 }
-function DonationViewComponent({apiUrl}: {apiUrl: string}) {
+function DonationViewComponent({ apiUrl }: { apiUrl: string }) {
   const { data: donations } = useQuery({
     queryKey: ['donations'],
     queryFn: async () => {
-      const response = await fetch(`${apiUrl}/api/donation/getall?page=1&limit=10`);
+      const response = await fetch(`${apiUrl}/api/donation/getall?page=1&limit=5`);
       const { data } = await response.json();
       return data as Donation[];
     },
   });
-  return <div className="flex flex-col gap-4 justify-center items-center">{donations?.map((donation: Donation, index: number) => <DonationCard key={index} {...donation} />)}</div>;
+  return <div className="flex flex-col p-4 rounded-lg gap-2 bg-white/20 backdrop-blur-sm items-center h-full overflow-y-auto shadow-lg">
+    <h1 className="text-2xl font-normal text-gray-800">5 Donasi terakhir</h1>
+    {donations?.map((donation: Donation, index: number) => <DonationCard key={index} {...donation} />)}
+  </div>;
 }
 
 function DonationCard({ donorName, amount, message }: { donorName: string; amount: number; message: string }) {
   return (
-    <div className="bg-white p-6 rounded-lg shadow-sm border border-gray-100 max-w-xl w-full">
-      <blockquote className="relative">
-        <p className="text-lg text-gray-800 italic leading-relaxed mb-4">
+    <div className="bg-white p-4 rounded-lg shadow-sm border border-gray-100 max-w-xl w-full">
+      <blockquote>
+        <p className="text-lg text-gray-800 italic leading-relaxed mb-2">
           "{message}"
         </p>
-        <footer className="mt-2">
-          <cite className="flex flex-col not-italic">
-            <span className="text-gray-700 font-medium">— {donorName}</span>
-            <span className="text-sm text-gray-500 mt-1">
-              Baru saja menyumbang Rp {amount.toLocaleString('id-ID')}
-            </span>
-          </cite>
-        </footer>
+        <p className="text-sm text-gray-500 mt-1">
+          <span className="text-gray-700 font-medium">— {donorName + " "}</span>
+
+          <span className="text-sm text-gray-500 mt-1">
+            baru saja menyumbang Rp {amount.toLocaleString('id-ID')}
+          </span>
+        </p>
       </blockquote>
     </div>
   );
 }
 
-export default function DonationView({apiUrl}: {apiUrl: string}) {
+export default function DonationView({ apiUrl }: { apiUrl: string }) {
   const queryClient = new QueryClient();
   return (
     <QueryClientProvider client={queryClient}>
